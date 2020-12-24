3d23160b0331eed5b2001aa9cc758f76a743b608
nicola.giacobbo <giacobbo.nicola@gmail.com>
2020-12-24 11:41:26 +0100

    some problem with pypi

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-  name = 'PiubRec',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'PubRec',
   version = '1.0',      
-  license='MIT',  
+  license ='MIT',  
   description = 'Simple python module to get the number of citations of a paper (or list of papers)',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Nicola Giacobbo',
   author_email = 'giacobbo.nicola@gmail.com', 
   url = 'https://github.com/GiacobboNicola/PubRec',   
   download_url = 'https://github.com/GiacobboNicola/PubRec/archive/v1.0.tar.gz', 
-  keywords = ['ADS', 'citations'],   
+  keywords = ['ADS', 'citations'],
+  packages=setuptools.find_packages(),
   install_requires=[        
           'bs4',
           'tqdm',
           'argaparse'
       ],
-  classifiers=[
+  classifiers = [
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
   ],
-)    
+)
