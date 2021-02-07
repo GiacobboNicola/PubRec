7395546b334c37c3fd18441fae301a60c9d9433a
nicola.giacobbo <giacobbo.nicola@gmail.com>
2021-02-07 18:45:54 +0100

    first version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
   name = 'PubRec',
-  version = '0.b',      
+  version = '1.0',      
   license ='MIT',  
   description = 'Simple python module to get the number of citations of a paper (or list of papers)',
   long_description='See the github `repository <https://github.com/GiacobboNicola/PubRec>`_.',
